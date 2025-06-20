bc893b5b0899eb699db8a28499a9ca44dab4a0b1
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-06-20 18:56:36 +0200

    chore: clean testing zig code

--- mlir-zig/src/root.zig
+++ mlir-zig/src/root.zig
@@ -4,15 +4,15 @@
 const std = @import("std");
 const testing = std.testing;
 
-const cmlir = @cImport({
+const c = @cImport({
     @cInclude("mlir-c/Support.h");
 });
 
 pub export fn add(a: i32, b: i32) i32 {
-    const res = cmlir.mlirLogicalResultSuccess();
+    const res = c.mlirLogicalResultSuccess(); // just some simple call to test the linkage
     return a + b + res.value;
 }
 
 test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    try testing.expect(add(3, 7) == 11);
 }
